85a24d4111a3f3ea3b918494773c8f461609b73a
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-07-01 10:41:34 +0200

    Pasar de css a styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,52 @@
 import React, { FC, useState } from "react";
-import "./App.css";
+import styled from "styled-components";
 
 const App: FC = () => {
-  const [listaPeliculas, setListaPeliculas] = useState("");
-  const handleClick = () => {
-    setListaPeliculas("Star Wars");
+  const [movieList, setMovieList] = useState("");
+  const handleClick = (event: any) => {
+    alert(movieList);
+  };
+
+  const handleChange = (event: any) => {
+    event.preventDefault();
+    setMovieList(event.target.value);
+    console.log(event.target.value);
   };
 
   return (
-    <div className="App">
-      <h1 className="title">Cinematik</h1>
-      <label htmlFor="titulo">Titulo:</label>
-      <input type="text" id="titulo" />
-      <button onClick={handleClick}>Guardar</button>
-      <div className="listadoPeliculas">{listaPeliculas}</div>
-    </div>
+    <StyledApp>
+      <Title>Cinematik</Title>
+      <form onSubmit={handleClick}>
+        <div>
+          <label htmlFor="titulo">Titulo:</label>
+          <TypeTitle onChange={handleChange} type="text" id="titulo" />
+          <button>Guardar</button>
+        </div>
+      </form>
+      <MovieList>{movieList}</MovieList>
+    </StyledApp>
   );
 };
 
 export default App;
+
+const StyledApp = styled.div`
+  padding: 30px;
+  background-color: antiquewhite;
+  text-align: center;
+  -webkit-box-shadow: 11px 10px 5px 0px rgba(0, 0, 0, 0.47);
+  -moz-box-shadow: 11px 10px 5px 0px rgba(0, 0, 0, 0.47);
+  box-shadow: 11px 10px 5px 0px rgba(0, 0, 0, 0.47);
+`;
+
+const MovieList = styled.div`
+  padding: 10px;
+`;
+
+const TypeTitle = styled.input`
+  margin-left: 15px;
+`;
+
+const Title = styled.h1`
+  text-transform: uppercase;
+`;
