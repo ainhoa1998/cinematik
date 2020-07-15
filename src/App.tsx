0d9f63d8e85a8ccbfb06fe38bf31e55ecd7945f8
Ainhoa Izkue <ainhoa.izkue@biko2.com>
2020-07-15 14:50:45 +0200

    Crea carpeta views

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { FC, useState } from "react";
 import styled from "styled-components";
-import { Button } from "./components/Button";
+import { Button } from "./views/components/Button";
 
 interface Movie {
   title: string;
